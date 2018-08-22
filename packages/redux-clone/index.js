3c6c4dfe48cb112c8c5e9efde7ce443f82852a96
Aashish Karki <aashish.y2z@gmail.com>
2018-08-22 16:39:44 +0545

    Update to phase two src

--- packages/redux-clone/index.js
+++ packages/redux-clone/index.js
@@ -1,4 +1,7 @@
-const createStore  = require('./src/createStore');
+const createStore = require('./src/createStore');
+const combineReducer = require('./src/combineReducer');
+
 module.exports = {
   createStore,
-}
+	combineReducer,
+};
