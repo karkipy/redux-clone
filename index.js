f02b8ddee459d3fdde00d3f972c0e276c37ef5c1
Aashish Karki <aashish.y2z@gmail.com>
2018-08-28 22:33:19 +0545

    Added provider and connect functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const createStore = require('./src/createStore');
 const combineReducer = require('./src/combineReducer');
+const { Provider } = require('./src/Provider');
 
 module.exports = {
   createStore,
   combineReducer,
+  Provider,
 };
