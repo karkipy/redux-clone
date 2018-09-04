0e42499b04ca0c7569353715c3e367e4e6f1a733
Aashish Karki <aashish.y2z@gmail.com>
2018-09-04 17:45:32 +0545

    add babel to the project

--- index.js
+++
@@ -1,9 +0,0 @@
-const createStore = require('./src/createStore');
-const combineReducer = require('./src/combineReducer');
-const { Provider } = require('./src/Provider');
-
-module.exports = {
-  createStore,
-  combineReducer,
-  Provider,
-};
